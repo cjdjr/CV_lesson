2bb10f54b7db27fb2e696fc2585bd59c5af7f7df
cjdjr <2609943977@qq.com>
2019-06-16 23:02:35 +0800

    first commit

--- lab3/CV-3-E31614035.cpp
+++ lab3/CV-3-E31614035.cpp
@@ -7,55 +7,180 @@
 #include <cstdlib>
 #include <iostream>
 #include "EigenFeature.hpp"
+#define mp make_pair
 using namespace cv;
 using namespace std;
-const int NUM_EIGEN_FACES=10;
+EigenFeature lefteye(3),righteye(3),nose(3),leftmouth(1),rightmouth(1);
+vector<Mat> testpicture;
+vector<vector<pair<int,int>>> testlabel;
+const double THRESHOLD=0.4;
+void readTestdata(string dirName)
+{
+    string pattern_jpg = dirName+"*.jpg";
+    string pattern_json = dirName+"*.json";
+    vector<cv::String> image_files;
+    vector<cv::String> image_jsons;
+    cv::glob(pattern_jpg, image_files);
+    cv::glob(pattern_json,image_jsons);
+    testpicture.resize(image_files.size());
+    testlabel.resize(image_files.size());
+    for(auto &x:testlabel) x.resize(8);
+    for(int i=0;i<image_files.size();++i)
+    {
+        std::ifstream in(image_jsons[i]);
+        std::ostringstream t;
+        t << in.rdbuf();
+        std::string content = t.str();
+        Document document;
+        document.Parse<0>(content.c_str());
+
+        for(int j=0;j<8;++j)
+        {
+            int y=int(document["shapes"][j]["points"][0].GetArray()[0].GetFloat());
+            int x=int(document["shapes"][j]["points"][0].GetArray()[1].GetFloat());
+            testlabel[i][j]=make_pair(x,y);
+        }
+
+        Mat tmp=imread(image_files[i],IMREAD_GRAYSCALE);
+        GaussianBlur(tmp,tmp,Size(7,7),0,0);
+        tmp.copyTo(testpicture[i]);
+    }
+}
+double IoU(pair<int,int> a,pair<int,int> b,pair<int,int> c,pair<int,int> d)
+{
+    int X=(b.first-a.first+1)*(b.second-a.second+1);
+    int Y=(d.first-c.first+1)*(d.second-c.second+1);
+    int I=max(0,min(b.first-c.first,d.first-a.first))*max(0,min(b.second-c.second,d.second-a.second));
+    return 1.0*I/(X+Y-I);
+}
+bool solve(EigenFeature& feature,Mat &origin,pair<int,int> &leftup,pair<int,int> &rightdown,int id)
+{
+    Mat test=origin.clone();
+    int nowx=test.rows;
+    int nowy=test.cols;
+    int ansx=-1,ansy=-1,xx=-1,yy=-1;
+    double mi=1000000000;
+    for(int i=0;i<30;++i)
+    {
+        if(3*feature.height>=nowx&&3*feature.width>=nowy) break;
+
+        int tmpx,tmpy;
+        //cout<<"enter test"<<endl;
+        double value=feature.match(test,0,tmpx,tmpy);
+        //cout<<"ok"<<endl;
+        //cout<<"value="<<value<<endl;
+        //cout<<"end test"<<endl;
+        if(15*feature.height>=nowx&&15*feature.width>=nowy)
+            if(value<mi) mi=value,ansx=nowx,ansy=nowy,xx=tmpx,yy=tmpy;
+        nowx=0.9*nowx,nowy=0.9*nowy;
+        resize(origin,test,Size(nowy,nowx));
+    }
+
+   // resize(origin,test,Size(ansy,ansx));
+    //feature.match(test,1,xx,yy);
+
+    int height=feature.height*origin.rows/ansx;
+    int width=feature.width*origin.cols/ansy;
+    ansx=xx*origin.rows/ansx;
+    ansy=yy*origin.cols/ansy;
+
+
+//    cout<<"ansx="<<ansx<<endl;
+//    cout<<"ansy="<<ansy<<endl;
+//    Point matchLoc(ansy,ansx);
+//    cout<<matchLoc.x<<endl;
+//    cout<<matchLoc.y<<endl;
+//    Mat paint=origin.clone();
+//    circle(paint, Point(matchLoc.x + width/2, matchLoc.y + height/2), 2, Scalar(0, 0, 255),-1);//在图像中画出特征点，2是圆的半径
+//    rectangle(paint, matchLoc, Point(matchLoc.x + width, matchLoc.y + height), Scalar(0, 255, 0), 1, 1, 0);
+//    imshow("img", paint);
+//    cout<<"distance : "<<mi<<endl;
+
+    double value=IoU(mp(ansx,ansy),mp(ansx+height-1,ansy+width-1),leftup,rightdown);
+        Point matchLoc(ansy,ansx);
+        Mat paint=origin.clone();
+        circle(paint, Point(matchLoc.x + width/2, matchLoc.y + height/2), 2, Scalar(0, 0, 255),-1);//在图像中画出特征点，2是圆的半径
+        rectangle(paint, matchLoc, Point(matchLoc.x + width, matchLoc.y + height), Scalar(0, 255, 0), 1, 1, 0);
+        imwrite("rightmoutherror/"+to_string(id)+".jpg", paint);
+    if(value>=THRESHOLD) return 1;else return 0;
+
+}
 int main(int argc, char **argv)
 {
     string dirName="train/";
+    /*
+    readTemplates(dirName);
+    align_templ_size(height,width);
+    calEigen();*/
+    lefteye.readTemplates(dirName,0,1);
+    lefteye.align_templ_size(15,24);
+    lefteye.calEigen();
 
-    Mat test=imread("faceSamples/0598.jpg",IMREAD_GRAYSCALE);
-    GaussianBlur(test,test,Size(5,5),0,0);
-    //Mat test=imread("test.png",IMREAD_GRAYSCALE);
-    //cout<<test.rows<<endl;
+    righteye.readTemplates(dirName,2,3);
+    righteye.align_templ_size(15,24);
+    righteye.calEigen();
 
+    nose.readTemplates(dirName,4,5);
+    nose.align_templ_size(12,24);
+    nose.calEigen();
 
+    leftmouth.readTemplates(dirName,6,7);
+    leftmouth.align_templ_size(8,8);
+    leftmouth.calEigen();
 
-    int ansx=-1,ansy=-1;
-    double mi=1000000;
-    int nowx=25*3,nowy=43*3;
-    for(int i=0;i<30;++i)
+    rightmouth.readTemplates(dirName,8,9);
+    rightmouth.align_templ_size(8,8);
+    rightmouth.calEigen();
+
+    dirName="test/";
+    readTestdata(dirName);
+    //imshow("4",testpicture[3]);
+    int correct_lefteye=0;
+    int correct_righteye=0;
+    int correct_eye=0;
+    int correct_nose=0;
+    int correct_leftmouth=0;
+    int correct_rightmouth=0;
+    for(int i=0;i<testpicture.size();++i)
     {
-        if(nowx<=1.0*test.rows/15||nowy<=1.0*test.cols/8) break;
-        EigenFeature templ(3);
+        int ans;
+        cout<<"lefteye: "<<i<<endl;
+         ans=solve(lefteye,testpicture[i],testlabel[i][0],testlabel[i][1],i);
+        correct_lefteye+=ans;
+        if(ans) cout<<"correct!"<<endl;else cout<<"error!"<<endl;
 
-        templ.readTemplates(dirName);
-        templ.align_templ_size(nowx,nowy);
+        cout<<"rightteye: "<<i<<endl;
+         ans=solve(righteye,testpicture[i],testlabel[i][2],testlabel[i][3],i);
+        correct_righteye+=ans;
+        if(ans) cout<<"correct!"<<endl;else cout<<"error!"<<endl;
 
-        templ.calEigen();
+        cout<<"eye: "<<i<<endl;
+         ans=solve(lefteye,testpicture[i],testlabel[i][0],testlabel[i][1],i)|solve(righteye,testpicture[i],testlabel[i][2],testlabel[i][3],i);
+        correct_eye+=ans;
+        if(ans) cout<<"correct!"<<endl;else cout<<"error!"<<endl;
 
-        double value=templ.match(test,1);
+        cout<<"nose: "<<i<<endl;
+         ans=solve(nose,testpicture[i],testlabel[i][4],testlabel[i][5],i);
+        correct_nose+=ans;
+        if(ans) cout<<"correct!"<<endl;else cout<<"error!"<<endl;
 
-        if(value==1000000000000) break;
-        cout<<nowx<<" "<<nowy<<" "<<value<<endl;
-        if(nowx>=1.0*test.rows/15&&nowy>=1.0*test.cols/8)
-        {
-            if(value<mi) mi=value,ansx=nowx,ansy=nowy;
-        }
-        else
-            if(value<mi)
-                mi=value,ansx=nowx,ansy=nowy;
-        nowx=0.9*nowx,nowy=0.9*nowy;
+        cout<<"leftmouth: "<<i<<endl;
+         ans=solve(leftmouth,testpicture[i],testlabel[i][6],testlabel[i][7],i);
+        correct_leftmouth+=ans;
+        if(ans) cout<<"correct!"<<endl;else cout<<"error!"<<endl;
+
+        cout<<"rightmouth: "<<i<<endl;
+         ans=solve(rightmouth,testpicture[i],testlabel[i][8],testlabel[i][9],i);
+        correct_rightmouth+=ans;
+        if(ans) cout<<"correct!"<<endl;else cout<<"error!"<<endl;
+        //break;
     }
-
-    //66 115
-    EigenFeature templ(3);
-    templ.readTemplates(dirName);
-    templ.align_templ_size(ansx,ansy);
-    templ.calEigen();
-    templ.match(test,1);
-
-
+    cout<<"lefteye: "<<correct_lefteye<<endl;
+    cout<<"righteye: "<<correct_righteye<<endl;
+    cout<<"eye: "<<correct_eye<<endl;
+    cout<<"nose: "<<correct_nose<<endl;
+    cout<<"leftmouth: "<<correct_leftmouth<<endl;
+    cout<<"rightmouth: "<<correct_rightmouth<<endl;
 
     cout<<clock()<<endl;
     waitKey(0);
--- lab3/EigenFeature.hpp
+++ lab3/EigenFeature.hpp
@@ -26,27 +26,26 @@ struct EigenFeature
 {
 private:
     int NUM_EIGEN_FACES=3;
-    int height=100,width=100;
-    PCA pca;
 
+    PCA pca;
     Mat sigma;
     vector<Mat> templates;
     double calDifference(const Mat &img,int);
     Mat createDataMatrix();
 
 public:
+    int height=15,width=24;
     EigenFeature(){}
     EigenFeature(int num):NUM_EIGEN_FACES(num){} ;
-    void readTemplates(string dirName);
+    void readTemplates(string dirName,int leftup,int rightdown);
     void align_templ_size(int x,int y);
     void calEigen();
-    double match(const Mat& img,int is_show=0);
+    double match(const Mat& img,int is_show,int &xx,int &yy);
 
 };
 
-void EigenFeature::readTemplates(string dirName)
+void EigenFeature::readTemplates(string dirName,int leftup,int rightdown)
 {
-    //cout<<"reading templates..."<<endl;
     string pattern_jpg = dirName+"*.jpg";
     string pattern_json = dirName+"*.json";
     vector<cv::String> image_files;
@@ -56,36 +55,25 @@ void EigenFeature::readTemplates(string dirName)
     templates.resize(image_files.size());
     for(int i=0;i<image_files.size();++i)
     {
-        //cout << image_files[i] << endl;
         std::ifstream in(image_jsons[i]);
         std::ostringstream t;
         t << in.rdbuf();
         std::string content = t.str();
-        //content="{\"hello\" : \"ligoudan\"}";
-
         Document document;
         document.Parse<0>(content.c_str());
-
         int x1,y1,x2,y2;
-        //cout<<document["shapes"][0]["points"][0].GetArray()<<endl;
-
-        y1=int(document["shapes"][0]["points"][0].GetArray()[0].GetFloat());
-        x1=int(document["shapes"][0]["points"][0].GetArray()[1].GetFloat());
-        y2=int(document["shapes"][1]["points"][0].GetArray()[0].GetFloat());
-        x2=int(document["shapes"][1]["points"][0].GetArray()[1].GetFloat());
-
-        //cout<<x1<<" "<<y1<<" "<<x2<<" "<<y2<<endl;
-        //break;
+        y1=int(document["shapes"][leftup]["points"][0].GetArray()[0].GetFloat());
+        x1=int(document["shapes"][leftup]["points"][0].GetArray()[1].GetFloat());
+        y2=int(document["shapes"][rightdown]["points"][0].GetArray()[0].GetFloat());
+        x2=int(document["shapes"][rightdown]["points"][0].GetArray()[1].GetFloat());
         Mat tmp(imread(image_files[i],IMREAD_GRAYSCALE),Rect(y1,x1,y2-y1,x2-x1));
         tmp.copyTo(templates[i]);
     }
 }
-
 void EigenFeature::align_templ_size(int x,int y)
 {
     height=x,width=y;
     for(auto &x:templates) resize(x,x,Size(width,height));
-
     sigma=Mat(height,width,CV_32F);
     double S=0;
     for(int i=0;i<x;++i)
@@ -98,10 +86,8 @@ void EigenFeature::align_templ_size(int x,int y)
             for(auto x:templates) sum+=sqr(mean-x.at<uchar>(i,j));
             sum/=templates.size();
             sum=sqrt(sum);
-
             sum=1.0/sum;
             sigma.at<float>(i,j)=sum;
-            //cout<<i<<" "<<j<<" "<<sum<<endl;
             S+=sum;
         }
     for(int i=0;i<x;++i)
@@ -112,14 +98,12 @@ void EigenFeature::align_templ_size(int x,int y)
 
 Mat EigenFeature::createDataMatrix()
 {
-    //cout << "Creating data matrix from templates ..."<<endl;
     Mat data(static_cast<int>(templates.size()), templates[0].rows * templates[0].cols , CV_32F);
     for(int i = 0; i < templates.size(); i++)
     {
         Mat image = templates[i].reshape(1,1);
         image.copyTo(data.row(i));
     }
-    //cout << " DONE" << endl;
     return data;
 }
 
@@ -127,131 +111,48 @@ void EigenFeature::calEigen()
 {
     Size sz = templates[0].size();
     Mat data = createDataMatrix();
-    //cout << "Calculating PCA ...";
     pca=PCA(data, Mat(), PCA::DATA_AS_ROW, NUM_EIGEN_FACES);
-    //MeansTemplates=pca.mean;
-    //cout << " DONE"<< endl;
 
-    /*templates_pca.resize(templates.size());
-    for(int i=0;i<templates.size();++i)
-    {
-        templates_pca[i]=pca.project(templates[i].reshape(1,1));
-        //normlize(templates_pca[i],templates_pca[i],0,1,NORM_MINMAX)
-    }*/
-
-    /*Mat tmp=pca.mean.reshape(1,sz.height);
-    normalize(tmp,tmp,0,1,NORM_MINMAX,CV_32F);
-    imshow("wmr",tmp);*/
-
-    /*Mat tmp=pca.project(templates[0].reshape(1,1));
-    cout<<tmp<<endl;
-    Mat output=pca.backProject(tmp).reshape(1,templates[0].rows);
-    normalize(output, output, 0, 255, NORM_MINMAX, CV_8UC1);
-    imshow("haha",output);*/
-
-    //cout<<pca.backProject(tmp)<<endl;
-    //tmp=pca.project(templates[1].reshape(1,1));
-    //cout<<tmp<<endl;
-
-    /*Mat averageFace = pca.mean.reshape(1,sz.height);
-    Mat dst;
-    normalize(averageFace, dst, 0, 255, NORM_MINMAX, CV_8UC1);
-    Mat output(averageFace.size(),CV_8UC1);
-    for(int i=0;i<output.rows;++i)
-        for(int j=0;j<output.cols;++j)
-            output.at<uchar>(i,j)=int(averageFace.at<float>(i,j));*/
-    //resize(averageFace, output, Size(), 2, 2);
-    //imshow("Result", dst);
-    //imshow("Result1", output);
 }
 double EigenFeature::calDifference(const Mat &img,int is_show)
 {
-    //assert(height==img.rows&&width==img.cols);
-    //cout<<"enter..."<<endl;
-    //cout<<img<<endl;
     if(is_show)
         imshow("origin",img);
     Mat img1=img.clone();
-    //bilateralFilter (img1, img1, 3, 500, 1000);
-
     img1=img1.reshape(1,1);
     Mat tmp=pca.project(img1);
     Mat output=pca.backProject(tmp);
-    //cout<<tmp<<endl;
-    //cout<<output<<endl;
     if(is_show==1)
     {
         Mat tmp=output.clone().reshape(1,img.rows);
-        //cout<<img1<<endl;
-        //cout<<output<<endl;
         double sum=0;
-        /*for(int i=0;i<img.rows;++i)
-        {
-            for(int j=0;j<img.cols;++j)
-                cout<<tmp.at<float>(i,j)-img.at<uchar>(i,j)<<" ",sum+=sqr(tmp.at<float>(i,j)-img.at<uchar>(i,j));
-            cout<<endl;
-        }*/
         sum=sqrt(sum/(img.cols*img.rows));
         cout<<"sum="<<sum<<endl;
         normalize(tmp, tmp, 0, 1, NORM_MINMAX, -1, Mat());
         Mat imgshow=img.clone();
-        //normalize(imgshow,imgshow,0,1,NORM_MINMAX,-1,Mat());
         imshow("before_pca",imgshow);
         imshow("after_pca",tmp);
     }
-    //double s1=0,s2=0;
-    //double sigma1=0,sigma2=0;
     normalize(output,output,0,256,NORM_MINMAX,-1,Mat());
-
     normalize(img1,img1,0,256,NORM_MINMAX,-1,Mat());
     if(is_show==1)
     {
         for(int i=0;i<img.rows*img.cols;++i)
             cout<<output.at<float>(0,i)-img1.at<uchar>(0,i)<<endl;
     }
-    //for(int i=0;i<img.rows*img.cols;++i) s1+=output.at<float>(0,i),s2+=img1.at<uchar>(0,i);
-    //s1/=img.cols*img.rows;
-    //s2/=img.cols*img.rows;
-    //for(int i=0;i<img.rows*img.cols;++i) sigma1+=sqr(output.at<float>(0,i)-s1),sigma2+=sqr(1.0*img1.at<uchar>(0,i)-s2);
-    //sigma1/=img.cols*img.rows;
-    //sigma2/=img.cols*img.rows;
-    //sigma1=sqrt(sigma1);
-    //sigma2=sqrt(sigma2);
-    //cout<<output<<endl;
+
     double ans=0;
     for(int i=0;i<img.rows*img.cols;++i)
         ans+=sigma.at<float>(0,i)*abs(output.at<float>(0,i)-img1.at<uchar>(0,i));
-        //ans+=abs(output.at<float>(0,i)-img1.at<uchar>(0,i));
-    ans=(ans);
-    //cout<<ans<<endl;
+
     if(is_show==1) cout<<"value="<<ans<<endl;
     return ans;
-    //cout<<"wmr"<<endl;
-    /*double mi=1000000000;
-    for(auto &x:templates_pca)
-    {
-        double ans=0;
-       /* for(int i=0;i<tmp.cols;++i)
-            ans+=sqr(tmp.at<float>(0,i)-x.at<float>(0,i));
-        ans/=tmp.cols;
-        ans=sqrt(ans);
-        ans=-cv::compareHist(x, tmp, CV_COMP_CORREL);
-
-        mi=min(mi,ans);
-        //mi+=ans;
-    }*/
-   // cout<<sqrt(ans)<<endl;
-   // mi/=templates_pca.size();
 }
 
-double EigenFeature::match(const Mat &img,int is_show)
+double EigenFeature::match(const Mat &img,int is_show,int &xx,int &yy)
 {
-    /*Mat tmp=pca.project(templates[0].reshape(1,1));
-    cout<<tmp<<endl;
-    Mat output=pca.backProject(tmp).reshape(1,templates[0].rows);
-    normalize(output, output, 0, 255, NORM_MINMAX, CV_8UC1);
-    imshow("haha",output);*/
-    cout<<height<<" "<<img.rows<<" "<<width<<" "<<img.cols<<endl;
+
+    //cout<<height<<" "<<img.rows<<" "<<width<<" "<<img.cols<<endl;
     if(height>img.rows||width>img.cols) return 1000000000000;
 
     int ansx=-1,ansy=-1;
@@ -264,17 +165,13 @@ double EigenFeature::match(const Mat &img,int is_show)
             //if(i==110&&j==103) tmp=calDifference(Mat(img,Rect(j,i,width,height)),1);
             //else
             tmp=calDifference(Mat(img,Rect(j,i,width,height)),0);
-
             //if(i==228&&j==91) imshow("cnm",Mat(img,Rect(j,i,width,height)));
             value.at<float>(i,j)=(float)tmp;
             if(tmp<mi) mi=tmp,ansx=i,ansy=j;
             //break;
         }
-    //cout<<value<<endl;
-    //normalize(value, value, 0, 1, NORM_MINMAX, -1, Mat());
-    //imshow("value",value);
-    //cout<<value<<endl;
 
+    xx=ansx,yy=ansy;
     if(is_show)
     {
         cout<<"ansx="<<ansx<<endl;
